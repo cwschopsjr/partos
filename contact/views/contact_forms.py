b73145f9b2445814258cceda693c4e97f389e54a
cwschopsjr <cwschops.jr@gmail.com>
2024-02-29 11:56:07 -0300

    Criação da classe ContactForm e atualizações no template create

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,13 +2,33 @@ from django.shortcuts import get_object_or_404, render, redirect
 from contact.models import Contact
 from django.db.models import Q
 from django.core.paginator import Paginator
+from typing import Any, Dict
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'data_de_internacao', 'nome_da_gestante', 'data_nascimento_gestante',
+        )
 
 
 def create(request):
-    post = request.POST
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
 
-    context = {
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
 
-    }
+    context = {'form': ContactForm()
+
+               }
 
     return render(request, 'contact/create.html', context)
